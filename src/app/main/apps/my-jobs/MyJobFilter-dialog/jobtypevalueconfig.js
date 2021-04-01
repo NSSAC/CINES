5d4f24738dabe53165d82cd380f016ca0e32ee26
Abhishek Beohar <abhishek_beohar@persistent.co.in>
2021-04-01 12:20:28 +0530

    added all the status in filter

--- src/app/main/apps/my-jobs/MyJobFilter-dialog/jobtypevalueconfig.js
+++ src/app/main/apps/my-jobs/MyJobFilter-dialog/jobtypevalueconfig.js
@@ -1,6 +1,6 @@
 const JOBTYPEVALUE = {
 
-    statusType: ['Completed', 'Failed', 'Cancelled', 'Running'],
+    statusType: ['Completed', 'Failed', 'Cancelled', 'Running','UserQueued','Queued','Held','UserHeld'],
     jobDefinationType: ['epihiper_db_service', 'epihiper_cell_simulation', 'epihiper_multicell_analysis']
 
 
