f5305b2e3e8190acf28ca7f20f87179e0b621e76
Sayantan Mondal <sayantan_mondal@persistent.com>
2022-01-04 18:28:12 +0530

    Implementation of Dynamic Form (V3) creation from CSonNet_modelDefinition_v3.js

---
+++ src/app/main/apps/job-definition/Job-Definition/static-forms/CSonNet_Contagion_Simulation/v3/submodels.js
@@ -0,0 +1,105 @@
+import React, { useEffect } from 'react';
+import ReactTooltip from 'react-tooltip';
+
+import { SelectFormsy, TextFieldFormsy } from '@fuse';
+
+import { MenuItem } from '@material-ui/core';
+
+export const Submodels = (props) => {
+    var renderField = null;
+    useEffect(() => {
+        ReactTooltip.rebuild();
+    })
+    if (props.sub.menu){
+        renderField = <SelectFormsy
+            className="my-12 inputStyle1 model"
+            name="Sub Model"
+            label={[
+                "Sub Model",
+                <span key={1} style={{ color: "red" }}>
+                    {"*"}
+                </span>,
+            ]}
+              value={props.sub.value}
+            onChange={props.changed}
+            required
+        >
+            {props.sub.menu.map((subModel) => {
+                return (
+                    <MenuItem key={subModel} value={subModel}>
+                        {subModel}
+                    </MenuItem>
+                );
+            })}
+        </SelectFormsy>}
+    else {
+        switch (props.sub[Object.keys(props.sub)[0]].type) {
+            case "integer":
+                renderField = <TextFieldFormsy
+                    className="my-16 inputStyle"
+                    type="text"
+                    name={props.sub[Object.keys(props.sub)[0]].label}
+                    style={{ width: "18px" }}
+                    value={props.sub.value}
+                    label={props.sub[Object.keys(props.sub)[0]].label}
+                    onChange={props.changed}
+                    validations={{
+                        isPositiveInt: function (values, value) {
+                            return RegExp(/^(?:[+]?(?:[0-9]\d*))$/).test(value);
+                        },
+                    }}
+                    validationError="This is not a valid value"
+                    autoComplete="off"
+                    required
+                />
+                break;
+            case "number":
+                renderField = <TextFieldFormsy
+                    className="my-16 inputStyle"
+                    type="text"
+                    name={props.sub[Object.keys(props.sub)[0]].label}
+                    style={{ width: "18px" }}
+                    value={props.sub.value}
+                    label={props.sub[Object.keys(props.sub)[0]].label}
+                    onChange={props.changed}
+                    validations={{
+                        isPositiveInt: function (values, value) {
+                            return RegExp(/^(0+\.?|0*\.\d+|0*1(\.0*)?)$/).test(value) 
+                        },
+                    }}
+                    validationError="This is not a valid value"
+                    autoComplete="off"
+                    required
+                />
+                break;
+            default:
+                renderField = <SelectFormsy
+                    className="my-12 inputStyle1 model"
+                    name="Sub Model"
+                    label={[
+                        "Sub Model",
+                        <span key={1} style={{ color: "red" }}>
+                            {"*"}
+                        </span>,
+                    ]}
+                    value={props.sub.value}
+                    onChange={props.changed}
+                    required
+                >
+                    {props.sub.menu.map((subModel) => {
+                        return (
+                            <MenuItem key={subModel} value={subModel}>
+                                {subModel}
+                            </MenuItem>
+                        );
+                    })}
+                </SelectFormsy>
+        }
+    }
+        return (
+            <>
+            {renderField}
+            {props.sub.description && props.desc(props.sub.description)}
+            </>
+        )
+}
