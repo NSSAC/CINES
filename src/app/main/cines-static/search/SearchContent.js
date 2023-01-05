ba22e9cdd4415c732bc47f5d5c92c9ac5e2e6abd
Sahil Narkhade <sahil_narkhade@persistent.com>
2023-01-05 12:31:52 +0530

    nameclick to file

--- src/app/main/cines-static/search/SearchContent.js
+++ src/app/main/cines-static/search/SearchContent.js
@@ -324,7 +324,9 @@ const SearchContent = () => {
                                     <Typography className="typoContentStyle">
                                        {device ? <Link to={`/files/${item.id}`}>
                                     {item.metadata.name}
-                                      </Link> : item.metadata.name}
+                                    </Link> : <Link style={{color:"black"}} underline="none"  to={`/files/${item.id}`}>
+                                    {item.metadata.name}
+                                      </Link>}
                                     </Typography>
                                   </Grid>
 
